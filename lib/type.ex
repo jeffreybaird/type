4c449c086ab2fab70117ab28d6f97c8e480aa9b2
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-10-21 14:11:58 -0700

    changing the order so that the last thing we check is whether or not it is an atom

--- lib/type.ex
+++ lib/type.ex
@@ -1,12 +1,12 @@
 defmodule Type do
   @spec check(any()) :: char_list
   def check(arg), do: _check(arg)
+  defp _check(arg) when is_nil(arg), do: 'Nil'
+  defp _check(arg) when is_boolean(arg), do: 'Boolean'
   defp _check(arg) when is_map(arg), do: 'Map'
   defp _check(arg) when is_list(arg), do: 'List'
-  defp _check(arg) when is_atom(arg), do: 'Atom'
   defp _check(arg) when is_binary(arg), do: 'Binary'
   defp _check(arg) when is_bitstring(arg), do: 'Bitstring'
-  defp _check(arg) when is_boolean(arg), do: 'Boolean'
   defp _check(arg) when is_float(arg), do: 'Float'
   defp _check(arg) when is_function(arg), do: 'Function'
   defp _check(arg) when is_integer(arg), do: 'Integer'
@@ -15,5 +15,5 @@ defmodule Type do
   defp _check(arg) when is_port(arg), do: 'Port'
   defp _check(arg) when is_reference(arg), do: 'Reference'
   defp _check(arg) when is_tuple(arg), do: 'Tuple'
-  defp _check(arg) when is_nil(arg), do: 'Nil'
+  defp _check(arg) when is_atom(arg), do: 'Atom'
 end
